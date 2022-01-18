23402e1ac62ab07ead2c3d34e7c0c1fc7c58eea7
Roberta de Siqueira <designer.roberta@gmail.com>
2022-01-17 21:05:55 -0300

    Enviando Projeto Mais Pra Ti v1

--- Main.java
+++ Main.java
@@ -12,8 +12,7 @@ import util.ConversaoDados;
 import java.text.ParseException;;
 import java.util.Date;
 
-public class Main
-{
+public class Main {
     public static void main (String[] args) throws ParseException {
 
         Scanner sc = new Scanner(System.in);
